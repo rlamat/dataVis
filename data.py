2762597d0651a640020b0a0bd711087e9459ba25
Rémi Lamat <remi.lamat@gmail.com>
2024-06-15 14:36:13 +0200

    Valid application for data viz

--- data.py
+++ data.py
@@ -6,54 +6,34 @@ from typing import List
 import numpy as np
 
 
-lastTime = 0
-
-
 def getDatasFromTDFFinishers():
-
-    # Lire le fichier CSV
     file_path = 'tdf_finishers.csv'
     df = pd.read_csv(file_path, header=None)
-
     # Nommer les colonnes grace à la premiere ligne
     df.columns = df.iloc[0]
     df = df[1:]  # Enlever la première ligne qui contient les noms des colonnes
-
     # Nettoyer les colonnes si nécessaire
-    df['Time'] = df['Time'].str.replace('"', '')  # Enlever les guillemets des temps
-    df['Team'] = df['Team'].str.strip()  # Enlever les espaces en trop dans les noms des équipes
-
-    # Afficher les premières lignes pour vérifier
-    #print(df.head())
-
+    df['Time'] = df['Time'].str.replace('"', '')
+    df['Team'] = df['Team'].str.strip()
     return df
 
 
 def getDatasFromTDFStages():
-    # Lire le fichier CSV
     file_path = 'tdf_stages.csv'
     df = pd.read_csv(file_path, header=None)
-
     # Nommer les colonnes grace à la premiere ligne
     df.columns = df.iloc[0]
     df = df[1:]  # Enlever la première ligne qui contient les noms des colonnes
-
-    # Extraire le nombre de kilomètres de la colonne Distance et convertir en int
     df['Distance'] = df['Distance'].str.extract(r'(\d+)').astype(int)
- 
-    # Afficher les premières lignes pour vérifier
-    #print(df.head())
-    
     return df
 
 
 def extract_distance_km(distance_str):
-    # Utiliser une expression régulière pour extraire le nombre de kilomètres
     match = re.search(r'(\d+) km', distance_str)
     if match:
         return int(match.group(1))
     else:
-        return None  # Gestion du cas où la distance n'est pas trouvée
+        return None
 
 
 def deleteCaracter(string):
@@ -62,32 +42,23 @@ def deleteCaracter(string):
 
 
 def getDatasFromTDFTours():
-    # Lire le fichier CSV
     # Détection de l'encodage du fichier CSV
     with open('tdf_tours.csv', 'rb') as f:
         result = chardet.detect(f.read())
 
     # Charger le fichier CSV en utilisant l'encodage détecté
     df = pd.read_csv('tdf_tours.csv', encoding=result['encoding'])
-     
-    # Appliquer la fonction extract_distance_km à la colonne 'Distance'
     df['Distance'] = df['Distance'].apply(deleteCaracter)
     df['Distance'] = df['Distance'].str.extract(r'(\d+)').astype(int)
-
-    # Afficher les premières lignes pour vérifier
-    #print(df.head())
-
     return df
 
 
 def convert_to_hours_float(time_str):
-    # Utiliser une expression régulière pour extraire heures, minutes et secondes
     match = re.match(r"(\d+)h\s*(\d+)'?\s*(\d+)\"?", time_str)
     if match:
         hours = int(match.group(1))
         minutes = int(match.group(2))
         seconds = int(match.group(3))
-        # Convertir en heures
         total_hours = hours + minutes / 60 + seconds / 3600
         return total_hours
     else:
@@ -95,45 +66,39 @@ def convert_to_hours_float(time_str):
     
     
 def keepOnlyNumberAndSpace(value):
-    # Vérifier si la valeur est un float
     if isinstance(value, float):
-        return str(value)  # Convertir le float en chaîne de caractères
-    
-    # Utiliser une expression régulière pour garder uniquement les chiffres et les espaces
+        return str(value)
     return re.sub(r'[^\d\s]', '', value)
 
 
 def graphNumberOfFinisher(df, startYear, endYear, compareStarters):
-    #draw a graph who show the number of finishers by year
-    #for each year count tne number of Rider where there is a value in the column 'Time'
     df['Year'] = df['Year'].astype(int)
     df = df.loc[df['Year'] >= startYear]
     df = df.loc[df['Year'] <= endYear]
-    print(df)
-    #do a graph with the number of starters and finishers by year
     if (compareStarters):
         fig = px.bar(df, x='Year', y=['Finishers', 'Starters'], title='Number of starters and finishers by year')
     else:
         fig = px.bar(df, x='Year', y='Finishers', title='Number of finishers by year')
-    #fig.show()
     return fig
 
 
-def graphAverageSpeed(dfFinishers, dfTours):
+def graphAverageSpeed(dfFinishers, dfTours, startYear, endYear):
     # Copie du DataFrame dfFinishers pour éviter de modifier l'original
     dfWinners = dfFinishers.copy()
     
-    # Assurez-vous que 'Rank' est numérique (convertissez si nécessaire)
+    # Assurance que 'Rank' est numérique (converti si nécessaire)
     dfWinners['Rank'] = pd.to_numeric(dfWinners['Rank'], errors='coerce')
     
-    # Filtrer les lignes où 'Rank' est égal à 1
+    # Filtrer
     df_filtered = dfWinners.loc[dfWinners['Rank'] == 1]
-    
-    # Sélectionner les colonnes pertinentes (Year, Rider, Time)
     dfWinners = df_filtered[['Year', 'Rider', 'Time']]
     
     # delete the line when the time is null
     dfWinners = dfWinners.dropna(subset=['Time'])
+    
+    dfWinners['Year'] = dfWinners['Year'].astype(int)
+    dfWinners = dfWinners.loc[dfWinners['Year'] >= startYear]
+    dfWinners = dfWinners.loc[dfWinners['Year'] <= endYear]
     
     # Convertir les temps en heures
     dfWinners['TimeInHours'] = dfWinners['Time'].apply(convert_to_hours_float)
@@ -143,22 +108,14 @@ def graphAverageSpeed(dfFinishers, dfTours):
     dfToursFiltered['Year'] = dfToursFiltered['Year'].astype(int)
     dfWinners['Year'] = dfWinners['Year'].astype(int)
     
-    # Merge les deux dataframes
     dfWinners = pd.merge(dfWinners, dfToursFiltered, on='Year')
-    
-    # distance moyenne
     dfWinners['AverageSpeed'] = dfWinners['Distance'] / dfWinners['TimeInHours']
     
-    # Afficher le résultat pour vérification
-    print(dfWinners)
-    
-    #draw a graph who show the average speed by year
     fig = px.line(dfWinners, x='Year', y='AverageSpeed', title='Average Speed of Tour de France Winners')
-    fig.show()
-    
+    fig.update_yaxes(title_text='Average Speed (km/h)')
+    return fig    
     
 def convert_to_hours(time_str):
-    # match = re.match(r"(\d+)h\s*(\d+)'?\s*(\d+)\"?", time_str)
     match = re.match(r"(?:(\d+)h\s*)?(?:(\d+)'\s*)?(\d+)\"?", time_str)
     hours = 0
     minutes = 0
@@ -176,52 +133,39 @@ def convert_to_hours(time_str):
         return 0.0
 
 
-def graphTimeOfFirst(dfFinishers, year=1913):
+def graphTimeOfFirst(dfFinishers, year, getFirst, getSecond, getThird, timeLadder):
     # Copie du DataFrame dfFinishers pour éviter de modifier l'original
     dfTop3 = dfFinishers.copy()
     
     # Assurez-vous que 'Rank' est numérique (convertissez si nécessaire)
     dfTop3['Rank'] = pd.to_numeric(dfTop3['Rank'], errors='coerce')
     
-    # Filtrer les lignes où 'Rank' est égal à 1
     dfTop3 = dfTop3.loc[dfTop3['Rank'] <= 3]
+    if (getFirst == False):
+        dfTop3 = dfTop3.loc[dfTop3['Rank'] != 1]
+    if (getSecond == False):
+        dfTop3 = dfTop3.loc[dfTop3['Rank'] != 2]
+    if (getThird == False):
+        dfTop3 = dfTop3.loc[dfTop3['Rank'] != 3]
     
     # delete the line when the time is null
     dfTop3 = dfTop3.dropna(subset=['Time'])
     
     # Convertir les temps en heures
     dfTop3['TimeInHours'] = dfTop3['Time'].apply(keepOnlyNumberAndSpace)
-    #dfTop3['Test'] = dfTop3['TimeInHours'].apply(convert_to_hours)d
     dfTop3Time = dfTop3['Time']
     dfTop3TimeArray = np.array(dfTop3Time, dtype=str)
     taille = len(dfTop3TimeArray)
     firstTime = 0.0
-    #for time in dfTop3['Time']:
-        #if (time[0] == '+'):
-            #time = deleteCaracter(time)
-            #time = convert_to_hours(time) + firstTime
-            #dfTop3['TimeInHours'] = time
-        #else:
-            #time = convert_to_hours(time)
-            #firstTime = time
-            #dfTop3['TimeInHours'] = time
     for i in range(taille):
         if (dfTop3TimeArray[i][0] == '+'):
             length = len(dfTop3TimeArray[i])
             time_str = dfTop3TimeArray[i][2:length]
-            #print("")
-            #print("temps :", time_str)
-            #print("convertion : ", convert_to_hours(time_str))
-            #print("temps premier : ", firstTime)
             time = convert_to_hours(time_str) + firstTime
             dfTop3TimeArray[i] = time
-            #print(time)
-            
         else:
-            #print("")
             time = convert_to_hours(dfTop3TimeArray[i])
             dfTop3TimeArray[i] = time
-            #print(time)
             firstTime = time
             
     #retransform the array in a dataframe
@@ -232,15 +176,67 @@ def graphTimeOfFirst(dfFinishers, year=1913):
     dfTop3ForOneYear['Year'] = dfTop3ForOneYear['Year'].astype(int)
     dfTop3ForOneYear = dfTop3ForOneYear.loc[dfTop3ForOneYear['Year'] == year]
     
-    print(dfTop3ForOneYear)
-    #draw a bar graph who show the time of the first 3 by year, the y origin is 0
-    fig = px.bar(dfTop3ForOneYear, x='Rider', y='TimeInHours', color='Rank', title='Time of the first 3 of the Tour de France ' + str(year))
-    #set the y origin to 0
-    fig.update_yaxes(range=[dfTop3ForOneYear['TimeInHours'].min()-3, 
+    if timeLadder == 'minute':
+        dfTop3ForOneYear['TimeInMin'] = dfTop3ForOneYear['TimeInHours'] * 60
+        fig = px.bar(dfTop3ForOneYear, x='Rider', y='TimeInMin', color='Rank', title='Time of the first 3 of the Tour de France ' + str(year))
+        fig.update_yaxes(range=[dfTop3ForOneYear['TimeInMin'].min()-3, 
+                            dfTop3ForOneYear['TimeInMin'].max()+2])
+    elif timeLadder == 'second':
+        dfTop3ForOneYear['TimeInSec'] = dfTop3ForOneYear['TimeInHours'] * 3600
+        fig = px.bar(dfTop3ForOneYear, x='Rider', y='TimeInSec', color='Rank', title='Time of the first 3 of the Tour de France ' + str(year))
+        fig.update_yaxes(range=[dfTop3ForOneYear['TimeInSec'].min()-3, 
+                            dfTop3ForOneYear['TimeInSec'].max()+2])
+    else :
+        fig = px.bar(dfTop3ForOneYear, x='Rider', y='TimeInHours', color='Rank', title='Time of the first 3 of the Tour de France ' + str(year))
+        fig.update_yaxes(range=[dfTop3ForOneYear['TimeInHours'].min()-3, 
                             dfTop3ForOneYear['TimeInHours'].max()+2])
-    fig.show()
+    return fig
 
-  
-dfFinishers = getDatasFromTDFFinishers()
-dfStages = getDatasFromTDFStages()
-dfTours = (getDatasFromTDFTours())
+
+def graphMultilineTimeOfFirst(dfFinishers, startYear, endYear, timeLadder):
+    # Copie du DataFrame dfFinishers pour éviter de modifier l'original
+    dfTop3 = dfFinishers.copy()
+    
+    # Assurance que 'Rank' est numérique (converti si nécessaire)
+    dfTop3['Rank'] = pd.to_numeric(dfTop3['Rank'], errors='coerce')
+    dfTop3 = dfTop3.loc[dfTop3['Rank'] <= 3]
+    
+    # delete the line when the time is null
+    dfTop3 = dfTop3.dropna(subset=['Time'])
+    
+    # Convertir les temps en heures
+    dfTop3['TimeInHours'] = dfTop3['Time'].apply(keepOnlyNumberAndSpace)
+    dfTop3Time = dfTop3['Time']
+    dfTop3TimeArray = np.array(dfTop3Time, dtype=str)
+    taille = len(dfTop3TimeArray)
+    firstTime = 0.0
+    for i in range(taille):
+        if (dfTop3TimeArray[i][0] == '+'):
+            length = len(dfTop3TimeArray[i])
+            time_str = dfTop3TimeArray[i][2:length]
+            time = convert_to_hours(time_str) + firstTime
+            dfTop3TimeArray[i] = time
+        else:
+            time = convert_to_hours(dfTop3TimeArray[i])
+            dfTop3TimeArray[i] = time
+            firstTime = time
+            
+    
+    #retransform the array in a dataframe
+    dfTop3['TimeInHours'] = dfTop3TimeArray
+    dfTop3['TimeInHours'] = dfTop3['TimeInHours'].astype(float)
+    
+    dfTop3ForOneYear = dfTop3.copy()
+    dfTop3ForOneYear['Year'] = dfTop3ForOneYear['Year'].astype(int)
+    dfTop3ForOneYear = dfTop3ForOneYear.loc[dfTop3ForOneYear['Year'] >= startYear]
+    dfTop3ForOneYear = dfTop3ForOneYear.loc[dfTop3ForOneYear['Year'] <= endYear]
+    
+    if timeLadder == 'minute':
+        dfTop3ForOneYear['TimeInMin'] = dfTop3ForOneYear['TimeInHours'] * 60
+        fig = px.line(dfTop3ForOneYear, x='Year', y='TimeInMin', color='Rank', title='Time of the first 3 of the Tour de France between ' + str(startYear) + ' and ' + str(endYear))
+    elif timeLadder == 'second':
+        dfTop3ForOneYear['TimeInSec'] = dfTop3ForOneYear['TimeInHours'] * 3600
+        fig = px.line(dfTop3ForOneYear, x='Year', y='TimeInSec', color='Rank', title='Time of the first 3 of the Tour de France between ' + str(startYear) + ' and ' + str(endYear))
+    else :
+        fig = px.line(dfTop3ForOneYear, x='Year', y='TimeInHours', color='Rank', title='Time of the first 3 of the Tour de France between ' + str(startYear) + ' and ' + str(endYear))
+    return fig
--- dataVis.py
+++ dataVis.py
@@ -1,21 +1,12 @@
 import dash
-import dash_core_components as dcc
-import dash_html_components as html
+from dash import dcc
+from dash import html
 from dash.dependencies import Input, Output
-import plotly.express as px
-import pandas as pd
 from data import getDatasFromTDFFinishers, getDatasFromTDFStages, getDatasFromTDFTours
-from data import graphNumberOfFinisher, graphAverageSpeed, graphTimeOfFirst
+from data import graphNumberOfFinisher, graphAverageSpeed, graphTimeOfFirst, graphMultilineTimeOfFirst
 
 # Créer une instance de l'application Dash
 app = dash.Dash(__name__)
-
-# Charger et préparer les données
-df = pd.DataFrame({
-    "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
-    "Amount": [4, 1, 2, 2, 4, 5],
-    "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
-})
 
 dfFinishers = getDatasFromTDFFinishers()
 dfStages = getDatasFromTDFStages()
@@ -27,25 +18,9 @@ compareStarters = False
 # Définir la disposition de l'application
 app.layout = html.Div(children=[
     html.H1(children='Data Visualisation Application'),
-
-    html.Div(children='''
-        Select fruits to display:
-    '''),
-
-    dcc.Dropdown(
-        id='fruit-dropdown',
-        options=[
-            {'label': 'Apples', 'value': 'Apples'},
-            {'label': 'Oranges', 'value': 'Oranges'},
-            {'label': 'Bananas', 'value': 'Bananas'}
-        ],
-        value=['Apples', 'Oranges', 'Bananas'],
-        multi=True
-    ),
-
-    dcc.Graph(
-        id='example-graph'
-    ),
+    
+    # Graphique pour le rapport finishers / starters
+    html.H2(children='Ratio Finishers / Starters by year graph'),
     
     html.Div(children='''
         Select year start to display the number of finishers:
@@ -63,24 +38,107 @@ app.layout = html.Div(children=[
         options=[
             {'label': 'Show Starters', 'value': 'show_starters'},
         ],
-        value=[],  # Default value
+        value=[],
         labelStyle={'display': 'inline-block'}
     ),
     
     dcc.Graph(
         id='finisher-graph'
-    )
-])
+    ),
+    
+    # Graphique pour la vitesse moyenne
+    html.H2(children='Average speed by year graph'),
+    
+    html.Div(children='''
+        Select year start to display the number of finishers:
+    '''),
+    dcc.Input(id='start-year-input_avgspeed', type='number', value=1903, min=1903, max=2022),
+    
+    html.Div(children='''
+        Select year end to display the number of finishers:
+    '''),
+    dcc.Input(id='end-year-input_avgspeed', type='number', value=2021, min=1903, max=2022),
+    
+    dcc.Graph(
+        id='avgspeed-graph'
+    ),
+    
+    # Graphique pour le temps des premiers
+    html.H2(children='Compare time of first riders for a year bar graph'),
+    
+    html.Div(children='''
+        Select year to display :
+    '''),
+    dcc.Input(id='yearToDisplay-input', type='number', value=1903, min=1903, max=2022),
+    
+    html.Div(children='''
+        Select riders to display:
+    '''),
 
-# Définir les callbacks pour mettre à jour le graphique en fonction de la sélection du dropdown
-@app.callback(
-    Output('example-graph', 'figure'),
-    Input('fruit-dropdown', 'value')
-)
-def update_graph(selected_fruits):
-    filtered_df = df[df['Fruit'].isin(selected_fruits)]
-    fig = px.bar(filtered_df, x="Fruit", y="Amount", color="City", barmode="group")
-    return fig
+    dcc.Dropdown(
+        id='timeOfFirsts-dropdown',
+        options=[
+            {'label': 'First Rider', 'value': 'first_rider'},
+            {'label': 'Second Rider', 'value': 'second_rider'},
+            {'label': 'Third Rider', 'value': 'third_rider'}
+        ],
+        value=['first_rider', 'second_rider', 'third_rider'],
+        multi=True
+    ),
+    
+    html.Div(children='''
+        Select time ladder:
+    '''),
+    
+    dcc.Dropdown(
+        id='time_Ladder-dropdown',
+        options=[
+            {'label': 'Hour', 'value': 'hour'},
+            {'label': 'Minute', 'value': 'minute'},
+            {'label': 'Second', 'value': 'second'}
+        ],
+        value='hour',
+        multi=False
+    ),
+
+    dcc.Graph(
+        id='timeOfFirsts-graph'
+    ),
+    
+    # Graphique pour l'écart entre les 3 premiers'
+    html.H2(children='Compare time of first riders by year multiline graph'),
+    
+    html.Div(children='''
+        Select year start to display:
+    '''),
+    dcc.Input(id='start_year-input', type='number', value=1903, min=1903, max=2022),
+    
+    html.Div(children='''
+        Select year end to display:
+    '''),
+    dcc.Input(id='end_year-input', type='number', value=2021, min=1903, max=2022),
+    
+    html.Div(children='''
+        Select time ladder:
+    '''),
+    
+    dcc.Dropdown(
+        id='timeLadder-dropdown',
+        options=[
+            {'label': 'Hour', 'value': 'hour'},
+            {'label': 'Minute', 'value': 'minute'},
+            {'label': 'Second', 'value': 'second'}
+        ],
+        value='hour',
+        multi=False
+    ),
+
+    dcc.Graph(
+        id='multilineFirsts-graph'
+    ),
+    
+    html.Button('Reset Zoom', id='reset-zoom-button', n_clicks=0)
+])
 
 
 @app.callback(
@@ -91,10 +149,56 @@ def update_graph(selected_fruits):
 )
 def update_finisher_graph(start_year, end_year, show_finishers_checklist):
     show_finishers = 'show_starters' in show_finishers_checklist
-    print(f"show_finishers: {show_finishers}")
     fig = graphNumberOfFinisher(dfTours, start_year, end_year, show_finishers)
     return fig
 
+
+@app.callback(
+    Output('avgspeed-graph', 'figure'),
+    [Input('start-year-input_avgspeed', 'value'),
+     Input('end-year-input_avgspeed', 'value')]
+)
+def update_avgspeed_graph(start_year, end_year):
+    fig = graphAverageSpeed(dfFinishers, dfTours, start_year, end_year)
+    return fig
+
+
+@app.callback(
+    Output('timeOfFirsts-graph', 'figure'),
+    Input('timeOfFirsts-dropdown', 'value'),
+    Input('yearToDisplay-input', 'value'),
+    Input('time_Ladder-dropdown', 'value')
+)
+def update_timeOfFirsts_graph(selected_riders, yearToDisplay, time_ladder):
+    firstRider = False
+    secondRider = False
+    thirdRider = False
+    if 'first_rider' in selected_riders:
+        firstRider = True
+    if 'second_rider' in selected_riders:
+        secondRider = True
+    if 'third_rider' in selected_riders:
+        thirdRider = True
+    fig = graphTimeOfFirst(dfFinishers, yearToDisplay, firstRider, secondRider, thirdRider, time_ladder)
+    return fig
+
+@app.callback(
+    Output('multilineFirsts-graph', 'figure'),
+    [Input('start_year-input', 'value'),
+     Input('end_year-input', 'value'),
+     Input('reset-zoom-button', 'n_clicks'),
+     Input('timeLadder-dropdown', 'value')]
+)
+def update_multilineFirsts_graph(start_year, end_year, n_clicks, time_ladder):
+    fig = graphMultilineTimeOfFirst(dfFinishers, start_year, end_year, time_ladder)
+    fig.update_layout(
+        title='Graphique interactif avec zoom',
+        xaxis_title='X Axis',
+        yaxis_title='Y Axis',
+        hovermode='closest',
+        dragmode='zoom',
+    )
+    return fig
 
 # Exécuter l'application
 if __name__ == '__main__':
